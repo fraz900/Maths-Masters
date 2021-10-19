196af294fc7acf5dba2b6afeb72812c02e52b5e4
fraz900 <74372649+fraz900@users.noreply.github.com>
2021-10-19 16:04:31 +0100

    Update encryption.py
    updated various functions to increase functionality (almost actually works)

--- server_stuff/encryption.py
+++ server_stuff/encryption.py
@@ -47,6 +47,7 @@ class information():
             final_line = []
             for byte in row:
                 new_byte = self._xor(byte,self.constant_matrix[counter1][counter2])
+                new_byte = byte
                 final_line.append(new_byte)
                 counter2 += 1
             final_matrix.append(final_line)
@@ -80,58 +81,69 @@ class information():
         return result
 
     def _decryptor(self,key,grid,counter):
+        print("ello")
         unkeyed = self._xor(grid,key)
-        matrix_holder = []
-        for i in range(0,len(unkeyed),32):
-            holder = unkeyed[i:i+32]
-            matrix_holder.append(holder)
-        matrix = []
-        for row in matrix_holder:
-            place = []
-            for i in range(0,len(row),8):
-                holder = row[i:i+8]
-                place.append(holder)
-            matrix.append(place)
-                
-        counter1 = 0
-        final_matrix = []
-        for row in matrix:
-            counter2 = 0
-            final_line = []
-            for byte in row:
-                new_byte = self._xor(byte,self.constant_matrix[counter1][counter2])
-                final_line.append(new_byte)
-                counter2 += 1
-            final_matrix.append(final_line)
-            counter1 += 1
+        #chunks = self._matrix(unkeyed,binary=True)
+        print("unkeyed",unkeyed)
+        thing = []
+        for i in range(0,len(unkeyed),8):
+            holder = unkeyed[i:i+8]
+            thing.append(holder)
+        print(thing,"thing")
+        chunks = []
+        for i in range(0,len(thing),4):
+            holder = thing[i:i+4]
+            chunks.append(holder)
+        print("chunks",chunks)
+        final = ""
+        for chunk in chunks:
+            matrix = []
+            for i in range(0,len(chunk),4):
+                holder = chunk[i:i+4]
+                matrix.append(holder)
+            counter1 = 0
+            final_matrix = []
+            for row in matrix:
+                counter2 = 0
+                final_line = []
+                for byte in row:
+                    new_byte = self._xor(byte,self.constant_matrix[counter1][counter2])
+                    new_byte = byte
+                    final_line.append(new_byte)
+                    counter2 += 1
+                final_matrix.append(final_line)
+                counter1 += 1
 
-        new_matrix = []
-        counter = 0
-        for line in matrix:
-            scale = len(line) - counter
-            new_line = self._rotate(line,scale)
-            new_matrix.append(new_line)
-            counter += 1
+            new_matrix = []
+            counter3 = 0
+            for line in matrix:
+                scale = len(line) - counter3
+                new_line = self._rotate(line,scale)
+                new_matrix.append(new_line)
+                counter3 += 1
 
-        concat = ""
-        for row in new_matrix:
-            for element in row:
-                concat += element
+            concat = ""
+            for row in new_matrix:
+                for element in row:
+                    concat += element
+            print(f"concat {concat}")
+            print()
+            final += concat
         if counter > 3:
-            return concat
+            return final
         counter += 1
-        result = self._decryptor(key,concat,counter)
+        result = self._decryptor(key,final,counter)
         return result
 
 
-            
-            
-            
         
-        #return result
-    
-    def _xor(self,binary1,binary2):
+
+    def _xor(self,binary1,binary2): #tick
         final = ""
+        if len(binary1) != len(binary2):
+            print("bin1",binary1)
+            print("bin2",binary2)
+            raise ValueError("values must be of same length")
         #both elements must be the same length
         for x in range(0,len(binary1)):
             num1 = binary1[x]
@@ -148,10 +160,14 @@ class information():
         return(final)
         
         
-    def _rotate(self,a_list,shift):
+    def _rotate(self,a_list,shift): #tick?
         return a_list[shift:] + a_list[:shift]
-    def _matrix(self,content):
-        binary_content = self._text2binary(content)
+
+    def _matrix(self,content,binary=False): #cannot take binary input (or can it? if decoded twice does it work?)
+        if not binary:
+            binary_content = self._text2binary(content)
+        else:
+            binary_content = content
         while len(binary_content) % 16 != 0:
             binary_content.append(self.padding)
         matrix = []
@@ -160,32 +176,36 @@ class information():
             matrix.append(chunk)
         return matrix
 
-    def _text2binary(self,string):
+    def _text2binary(self,string): #tick
         string = string.encode()
         value = []
         for byte in string:
             value.append(format(byte,"08b"))
         return(value)
+    def _binary2text(self,plaintext): #tick
+        letter = ""
+        final = ""
+        word = []
+        for digit in plaintext:
+            letter += digit
+            if len(letter) == 8:
+                word.append(letter)
+                letter = ""
+        for item in word:
+            item = int(item,2)
+            final += chr(item)
+        return final
 
 if __name__ == "__main__":
 
     a = information("this is a pretty cool test")
     key = a.generate_key()
-    a._matrix("this is quite the test")
-##    ciphertext = a.encrypt(key)
-##    print(ciphertext)
-##    print()
-##    plaintext = a.decrypt(key,ciphertext)
-##    print(f"decoded = {matrix}")
-##        
-##    letter = ""
-##    word = []
-##    for digit in plaintext:
-##        letter += digit
-##        if len(letter) == 8:
-##            word.append(letter)
-##            letter = ""
-##    for item in word:
-##        item = int(item,2)
-##        print(chr(item))
+    ciphertext = a.encrypt(key)
+    print(f"ciphertext: {ciphertext}")
+    print()
+    plaintext = a.decrypt(key,ciphertext)
+    print(f"decoded = {plaintext}")
+    print(a._binary2text(plaintext))
+
+    
 
