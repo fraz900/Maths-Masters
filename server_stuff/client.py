847b17372fa24397eb47ecfc25bc9b576b77b15a
fraz900 <74372649+fraz900@users.noreply.github.com>
2021-10-20 17:35:36 +0100

    Update client.py
    added additional functionality

--- server_stuff/client.py
+++ server_stuff/client.py
@@ -2,33 +2,52 @@ import socket
 
 class connection():
     def __init__(self):
+        #network things
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.SERVER_IP = "127.0.0.1" # server IP adress
-        self.PORT = 12345 #arbitrary value
-        self.AUTH = "11a3"
+        self.SERVER_IP = "127.0.0.1"
+        self.PORT = 12345
+        #commands
+        self.REFRESHAUTH_COMMAND = "rac"
+        self.REFRESHREFRESH_COMMAND = "rrc"
+        #responses
+        self.GOAHEAD = "200"
+        self.WARNINGS = {"400":"client error, incorrect command","401":"authentication error, failure to authenticate"}
 
-    def send_message(self,sock,message):
+        #other
+        self.REFRESH_CODE = "11a3"
+        
+    def _send_message(self,sock,message):
+        print(message)
         sock.sendall(message.encode())
 
     def get_auth_token(self):
-        commands = ["ra",self.AUTH]
+        commands = [self.REFRESHAUTH_COMMAND,self.REFRESH_CODE]
         self.s.connect((self.SERVER_IP,self.PORT))
         for command in commands:
             print(command)
             data = self.s.recv(1024)
             print(data.decode())
-            if data.decode() != "200":
-                print("no")
+            if data.decode() != self.GOAHEAD:
+                try:
+                    error = self.WARNINGS[data.decode()]
+                except KeyError:
+                    error = "UNKNOWN ERROR"
+                print("error")
+                raise Exception(error)
+            
                 return False
-            self.s.send(command.encode())
+            #self._send_message(self.s,command)
         data = self.s.recv(1024)
         return(data.decode())
-            
-        
-        
 
-c = connection()
-a = c.get_auth_token()
-print(a)
-#c.send_message(c.s,"test")
+
+
+
+if __name__ == "__main__":      
+    c = connection()
+    a = c.get_auth_token()
+    print(a)
+
+
+
 
