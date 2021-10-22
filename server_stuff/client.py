2c4f58b749236e556219bd9dfc238216c1eaa20d
fraz900 <74372649+fraz900@users.noreply.github.com>
2021-10-22 20:55:49 +0100

    Update client.py
    Implemented asymmetric key exchange at connection creation, implemented symmetric AES 128 encryption for all communications (aside from setup), fixed a bug with error handling

--- server_stuff/client.py
+++ server_stuff/client.py
@@ -1,6 +1,6 @@
 import socket
 import hashlib
-
+from encryption import DH,AES
 class connection():
     def __init__(self):
         #network things
@@ -18,29 +18,58 @@ class connection():
         #other
         self.REFRESH_CODE = "11a3"
         
-    def _send_message(self,sock,message):
+    def _send_message(self,sock,message,setup=False):
         print(f"sent : {message}")
-        sock.sendall(message.encode())
-
-    def _recieve_message(self,size=1024):
+        if setup:
+            sock.sendall(str(message).encode())
+        else:
+            a = AES(message)
+            encrypted_message = a.encrypt(self.key)
+            sock.sendall(encrypted_message.encode())
+            
+    def _recieve_message(self,size=1024,setup=False):
         data = self.s.recv(size)
         data = data.decode()
-        print(f"recieved : {data}")
-        return data
+        if setup:
+            print(f"recieved : {data}")
+            return data
+        else:
+            a = AES(data)
+            message = a.decrypt(self.key)
+            print(f"recieved : {message}")
+            return message
 
     def _error_handling(self,error):
+        error = str(error)
+        error = error.strip()
         try:
-            error = self.WARNINGS[data.decode()]
+            error = self.WARNINGS[error]
         except KeyError:
             error = "UNKNOWN ERROR"
         print("error")
         raise Exception(error)
     def _initiate_connection(self):
         self.s.connect((self.SERVER_IP,self.PORT))
-        data = self._recieve_message()
+        data = self._recieve_message(setup=True)
         if data != self.GOAHEAD:
             self._error_handling(data)
             return False
+
+        generate_key = True
+        if generate_key:
+            diffie = DH()
+            dhKey = diffie.generate_key()
+            modulus = diffie.generate_prime()
+            base = diffie.generate_base(modulus)
+            ag = diffie.equation(base,dhKey,modulus)
+            self._send_message(self.s,modulus,setup=True)
+            self._send_message(self.s,base,setup=True)
+            self._send_message(self.s,ag,setup=True)
+            bg = int(self._recieve_message(setup=True))
+            final = diffie.equation(bg,dhKey,modulus)
+            a = AES("")
+            self.key = a.produce_key(final)
+            #print("final",self.key)
     def get_auth_token(self):
         commands = [self.REFRESHAUTH_COMMAND,self.REFRESH_CODE]
         self._initiate_connection()
@@ -95,12 +124,11 @@ class communication():
 
 if __name__ == "__main__":      
     c = connection()
-    #a = c.get_auth_token()
-    #print(a)
-    c.create_account("test","ay")
+    a = c.get_auth_token()
+    print(a)
+    #c.create_account("test","ay")
 
 
 #TODO
-#get refresh code (expires after a week)
 #data uploading keeping scope in mind
 
