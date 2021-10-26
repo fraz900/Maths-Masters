25da4864074cfcbd6407bc37adad274bf0dd4d7b
fraz900 <74372649+fraz900@users.noreply.github.com>
2021-10-26 18:35:24 +0100

    Update client.py
    bugfixes, added upload functionality

--- server_stuff/client.py
+++ server_stuff/client.py
@@ -9,22 +9,37 @@ class connection():
         self.PORT = 12345
         #commands
         self.REFRESHAUTH_COMMAND = "rac"
-        self.REFRESHREFRESH_COMMAND = "rrc"
         self.CREATEACCOUNT = "ca"
+        self.UPLOADDATA = "ud"
         #responses
         self.GOAHEAD = "200"
-        self.WARNINGS = {"400":"client error, incorrect command","401":"authentication error, failure to authenticate"}
+        self.WARNINGS = {"400":"client error, incorrect command","401":"authentication error, failure to authenticate","500":"Data not allowed"}
 
         #other
-        self.REFRESH_CODE = "11a3"
+        try:
+            file = open("details.txt","r")
+            content = file.read()
+            file.close()
+            content = content.split(",")
+            self.REFRESH_CODE = content[1]
+            self.USER_NAME = content[0]
+        except:
+            self.REFRESH_CODE = None
+            self.USER_NAME = None
+
+        self.AUTHCODE = None
+        self.LARGESIZE = 20000
+        self.UPLOADS = "uploads.txt"
         
     def _send_message(self,sock,message,setup=False):
+        message = str(message)
         print(f"sent : {message}")
         if setup:
             sock.sendall(str(message).encode())
         else:
             a = AES(message)
             encrypted_message = a.encrypt(self.key)
+            print(self._size(encrypted_message))
             sock.sendall(encrypted_message.encode())
             
     def _recieve_message(self,size=1024,setup=False):
@@ -32,12 +47,12 @@ class connection():
         data = data.decode()
         if setup:
             print(f"recieved : {data}")
-            return data
+            return data.strip()
         else:
             a = AES(data)
             message = a.decrypt(self.key)
             print(f"recieved : {message}")
-            return message
+            return message.strip()
 
     def _error_handling(self,error):
         error = str(error)
@@ -46,8 +61,9 @@ class connection():
             error = self.WARNINGS[error]
         except KeyError:
             error = "UNKNOWN ERROR"
-        print("error")
         raise Exception(error)
+    def _size(self,s)->int:
+        return len(s.encode('utf-8'))
     def _initiate_connection(self):
         self.s.connect((self.SERVER_IP,self.PORT))
         data = self._recieve_message(setup=True)
@@ -63,36 +79,37 @@ class connection():
             base = diffie.generate_base(modulus)
             ag = diffie.equation(base,dhKey,modulus)
             self._send_message(self.s,modulus,setup=True)
+            self._recieve_message(setup=True)
             self._send_message(self.s,base,setup=True)
+            self._recieve_message(setup=True)
             self._send_message(self.s,ag,setup=True)
             bg = int(self._recieve_message(setup=True))
             final = diffie.equation(bg,dhKey,modulus)
             a = AES("")
             self.key = a.produce_key(final)
-            #print("final",self.key)
     def get_auth_token(self):
         commands = [self.REFRESHAUTH_COMMAND,self.REFRESH_CODE]
         self._initiate_connection()
         first = True
-        for command in commands:
-            if not first:
-                data = self._recieve_message()
-                if data != self.GOAHEAD:
-                    self._error_handling(data)
-                    return False
-            else:
-                first = False
-            self._send_message(self.s,command)
+        self._send_message(self.s,self.REFRESHAUTH_COMMAND)
         data = self._recieve_message()
-        return(data)
-
-
-
-
-
-
-
-    def create_account(self,username,password):#NEEDS FINISHING
+        data = data.strip()
+        if data != self.GOAHEAD:
+            self._error_handling(data)
+            return False
+        self._send_message(self.s,self.REFRESH_CODE)
+        data = self._recieve_message(size=self.LARGESIZE)
+        data = data.strip()
+        try:
+            test = self.WARNINGS[data]
+            self._error_handling(data)
+        except KeyError:
+            self.AUTHCODE = data
+            self.s.close()
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            return True
+        
+    def create_account(self,username,password):
         hasher = hashlib.sha256()
         hasher.update(password.encode())
         password = hasher.hexdigest()
@@ -107,28 +124,84 @@ class connection():
             self._send_message(self.s,username)
             self._send_message(self.s,password)
             user_test = self._recieve_message()
-            password_test = self._recieve_message()
+            password_test = self._recieve_message(size=self.LARGESIZE)
             if user_test == username and password_test == password:
                 correct = True
 
         self._send_message(self.s,self.GOAHEAD)
+        data = self._recieve_message()
+        if data == self.GOAHEAD:
+            entry = f"\n{username},{password}"
+            file = open("details.txt","w")
+            file.write(entry)
+            file.close()
+            self.REFRESH_CODE = password
+            self.USER_NAME = username
+            return True
+        else:
+            self._error_handling(data)
+
+            
+    def upload(self,data_to_send,name,recurse=False):#NOT DONE
+        if self.AUTHCODE == None:
+            self.get_auth_token()
+
+        auth = self.AUTHCODE
+        
+        self._initiate_connection()
+        self._send_message(self.s,self.UPLOADDATA)
+        data = self._recieve_message()
+        if data != self.GOAHEAD:
+            self._error_handling(data)
+            return False
+
+        self._send_message(self.s,auth)
+        confirm = self._recieve_message()
+        if confirm != self.GOAHEAD:
+            if recurse:
+                self._error_handling(confirm)
+                return False
+            else:
+                self.s.close()
+                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.AUTHCODE = None
+                self.upload(data,name,recurse=True)
+        size = self._size(data_to_send)
+        self._send_message(self.s,size)
+        self._recieve_message()
+        self._send_message(self.s,data_to_send)
+        check = self._recieve_message()
+        if check != self.GOAHEAD:
+            self._error_handling(check)
+        self._send_message(self.s,self.GOAHEAD)
+        
+        namer = self._recieve_message(size=self.LARGESIZE)
+        file = open(self.UPLOADS,"a")
+        entry = f"\n{name},{namer}"
+        file.write(entry)
+        file.close()
         return True
         
-        
-
-
 class communication():
     def __init__(self):
         None
 
+    def upload(self,data):
+        None
 
+    def update(self,change,name):
+        None
+
+    def delete(self,name):
+        None
 if __name__ == "__main__":      
     c = connection()
-    a = c.get_auth_token()
-    print(a)
-    #c.create_account("test","ay")
-
+    #a = c.get_auth_token()
+    #print(a)
+    #c.create_account("Fraz900","admin")
+    c.upload("this is a test","testing")
 
 #TODO
 #data uploading keeping scope in mind
+#limitted storage of auth codes to stop avoidable traffic
 
